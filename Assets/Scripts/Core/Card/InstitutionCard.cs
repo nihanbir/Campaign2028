1bbb10512aee3c251b92081f91bee45d4d9784d0
nihanbir <nihan.bir@gmail.com>
2025-10-18 22:39:13 +0200

    create institutioncard

---
+++ Assets/Scripts/Core/Card/InstitutionCard.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class InstitutionCard : Card
+{
+    public InstitutionType institutionType;
+    public List<int> redSuccessRolls = new List<int>();
+    public List<int> blueSuccessRolls = new List<int>();
+
+    public bool IsSuccessfulRoll(int roll, ActorTeam team)
+    {
+        List<int> successRolls = team == ActorTeam.Red ? redSuccessRolls : blueSuccessRolls;
+        return successRolls.Contains(roll);
+    }
+}
+public enum InstitutionType
+{
+    SupremeCourt,
+    Congress,
+    Media,
+    WhiteHouse,
+    Pentagon,
+    CIA
+}
