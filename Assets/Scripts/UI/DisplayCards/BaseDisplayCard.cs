84d5605854f780e506910d1da05bda0bfd7c06bf
nihanbir <nihan.bir@gmail.com>
2025-11-08 12:40:18 +0100

    try make ui better

--- Assets/Scripts/UI/DisplayCards/BaseDisplayCard.cs
+++ Assets/Scripts/UI/DisplayCards/BaseDisplayCard.cs
@@ -50,11 +50,9 @@ public class BaseDisplayCard<T> : MonoBehaviour, IDisplayCard where T : Card
         isResized = true;
         
         originalScale = transform.localScale;
-        originalSiblingIndex = transform.GetSiblingIndex();
+        
         
         // Move to front
-        transform.SetAsLastSibling();
-        
         transform.localScale = originalScale * movefrontScale;
     }
     
@@ -62,9 +60,6 @@ public class BaseDisplayCard<T> : MonoBehaviour, IDisplayCard where T : Card
     {
         if (!isResized) return;
         isResized = false;
-        
-        // Restore original sibling index
-        transform.SetSiblingIndex(originalSiblingIndex);
         
         transform.localScale = originalScale;
     }
